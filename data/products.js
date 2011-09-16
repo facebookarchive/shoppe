4edcf26bcd4d48e4066855a1acde6befc22d0654
David Pio <david@puffpio.com>
2011-09-16 00:11:27 +0000

    add images

--- data/products.js
+++ data/products.js
@@ -2,25 +2,25 @@ exports.Products = {
   airfilter: {
     name: 'HEPA air filter',
     description: 'Keeps your engine oh so pollen free',
-    thumb: 'airfilter.jpg',
+    image: 'airfilter.jpg',
     price: 20
   },
   exhaust: {
     name: 'Mega exhaust 9000',
     description: 'Makes your car sound angry bees in a tin can',
-    thumb: 'fartcan.jpg',
+    image: 'fartcan.jpg',
     price: 500
   },
   camshaft: {
     name: 'Ubertek Stage 2 camshaft',
     description: 'Amazing power without a lopey idle',
-    thumb: 'cams.jpg',
+    image: 'cams.jpg',
     price: 1000
   },
   coilovers: {
     name: 'Menske 4 way adjustable shocks',
     description: 'Independant compression and rebound adjustments with a threaded damper body',
-    thumb: 'coilover.jpg',
+    image: 'coilover.jpg',
     price: 3000
   }
 };
